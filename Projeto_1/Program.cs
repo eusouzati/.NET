1ed054e6b9072515c077c1b2dd3f823f0022d11f
Eusouzati <silvatiago.0407@gmail.com>
2021-03-29 16:01:12 -0300

    Projeto_1

---
+++ Projeto_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Projeto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: lista alunos
+                        break;
+                    case "3":
+                        //TODO: Calcular média 
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1- Inserir novo aluno ");
+            Console.WriteLine("2- Listar alunos ");
+            Console.WriteLine("3- Calcular média ")
+            Console.WriteLine("X - Sair")
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+}
